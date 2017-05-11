c41652bb7232aa4d53983d62d1121bbd3b3e2c2f
Artem Shvadskiy <shpen72991@gmail.com>
2017-05-11 01:24:16 -0400

    Generate project files

---
+++ desktop/src/com/artem/topdown/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.artem.topdown.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.artem.topdown.TopDownGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TopDownGame(), config);
+	}
+}
