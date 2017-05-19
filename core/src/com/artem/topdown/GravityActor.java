1fa84d997b613849d04c24a9604f1204b084c041
Artem Shvadskiy <shpen72991@gmail.com>
2017-05-19 10:45:35 -0400

    Make gravity actors attract or repel randomly

--- core/src/com/artem/topdown/GravityActor.java
+++ core/src/com/artem/topdown/GravityActor.java
@@ -13,7 +13,9 @@ public class GravityActor extends PhysicsActor {
 
     public GravityActor(World world, float x, float y) {
         super(new Texture("box.png"), x, y, world, BodyDef.BodyType.StaticBody, 0f);
-        mGravity = -10f;
+        boolean attract = Math.random() > 0.5f;
+        mGravity = 10f * (attract ? 1 : -1);
+        setColor(attract ? 1 : 0, 0, attract ? 0 : 1, 1);
     }
 
     public float getGravity() {
